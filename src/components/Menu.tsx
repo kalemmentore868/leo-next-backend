58c3c833f4b4224408c2bb9665ae95ac05d253bd
MeetMuaz <muazbalogun97@gmail.com>
2024-11-14 16:34:01 +0100

    menu component updated

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -77,9 +77,6 @@ const Menu = () => {
     <div className="mt-4 text-sm">
       {menuItems.map((i) => (
         <div className="flex flex-col gap-2" key={i.title}>
-          <span className="hidden lg:block text-gray-400 font-light my-4">
-            {i.title}
-          </span>
           {i.items.map((item) => {
               return (
                 <Link
